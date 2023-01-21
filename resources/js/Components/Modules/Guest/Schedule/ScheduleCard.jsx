d0685506ae7df42e3c16441c3c3d315a50b8e95f
Rachmizard <rachmizard11072000@gmail.com>
2023-01-21 23:42:31 +0700

    fix(): enhance schedule card

--- resources/js/Components/Modules/Guest/Schedule/ScheduleCard.jsx
+++ resources/js/Components/Modules/Guest/Schedule/ScheduleCard.jsx
@@ -4,7 +4,7 @@ import { TicketIcon } from "@heroicons/react/24/outline";
 import { format } from "date-fns";
 
 const formatDepartureDateToTime = (date) => {
-    return format(date, "hh:mm a");
+    return format(date, "HH:mm");
 };
 
 export default function ScheduleCard({ schedule = {}, onBooking }) {
@@ -14,7 +14,7 @@ export default function ScheduleCard({ schedule = {}, onBooking }) {
     const { from_outlet, to_outlet } = destination || {};
 
     return (
-        <div className="bg-white shadow-md rounded-md">
+        <div className="max-w-xl bg-white hover:bg-gray-100 transition duration-100 ease-in-out shadow-md rounded-md">
             <div className="px-4 py-4">
                 <div className="flex items-center justify-between">
                     <div className="flex items-center gap-2">
@@ -22,29 +22,29 @@ export default function ScheduleCard({ schedule = {}, onBooking }) {
                             <TicketIcon width={18} />
                         </div>
                         <div className="flex flex-col">
-                            <span className="text-sm font-medium text-gray-500">
+                            <span className="text-lg font-medium text-gray-500">
                                 {formatDepartureDateToTime(
                                     new Date(departure_date)
                                 )}
                             </span>
-                            <span className="text-xs font-medium text-gray-400">
+                            <span className="text-md font-medium text-gray-500">
                                 {from_outlet?.name} - {to_outlet?.name}
                             </span>
 
-                            <span className="text-xs font-medium text-gray-400">
+                            <span className="text-md font-medium text-gray-500">
                                 Tersedia {available_seats} kursi
                             </span>
                         </div>
                     </div>
 
                     <div className="flex flex-col gap-2">
-                        <span className="text-xs truncate font-bold text-gray-600">
+                        <span className="text-md truncate font-bold text-gray-600">
                             {formatRupiah(price)}
                         </span>
 
                         <Button
                             onClick={() => onBooking(schedule)}
-                            size="sm"
+                            size="md"
                             variant="outline"
                             colorScheme="blue"
                         >
--- resources/js/Components/Modules/Guest/Schedule/ScheduleResult.jsx
+++ resources/js/Components/Modules/Guest/Schedule/ScheduleResult.jsx
@@ -26,7 +26,7 @@ export default function ScheduleResult({ schedules = [], onBooking }) {
                 </Link>
             </div>
 
-            <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
                 {schedules.map((schedule, key) => (
                     <ScheduleCard
                         key={key}
